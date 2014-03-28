9c7a7ed4f51a3e1a2b9870e8b2bdb9c96f8d68d2
Shubhanshu Gupta <shubhanshu2007@gmail.com>
2014-03-29 04:31:47 +0530

    The extension page does'nt open up in a new tab when clicked again instead reloads all the current URLs in the same tab it was opened earlier

--- background.js
+++ background.js
@@ -1,5 +1,31 @@
+var flag = 0;
+
 chrome.browserAction.onClicked.addListener( function(tab) {
 
- chrome.tabs.create({url: 't2.html'});
+chrome.tabs.query({windowId:chrome.windows.WINDOW_ID_CURRENT},function(tabs){
+       // alert(tabs.length);
+       var size = tabs.length;
+       flag = 0;
+       for(i=0;i<size;i++){
+               if(tabs[i].title == "tAB++"){
+               	
+               	chrome.tabs.update(tabs[i].id,{selected:true, url:'t2.html'});
+                flag = 1;
+                        // alert("loop  "+flag);
+              //  console.log(flag);
+                break;
+               }
+       }
+       creat();
+       // alert(tabs[tabs.length-1].title);});
+});
+
+function creat(){
+       // alert(flag);
+        if(flag==0)
+                chrome.tabs.create({url: 't2.html'});
+}
+
+ //chrome.tabs.create({url: 't2.html'});
 
 });
