168e18a00f7f6351be177619acf48357e48e14e8
Saurav Mishra <g.saurav.mishra+git@gmail.com>
2014-03-29 06:35:40 +0530

    Tab and Window object defination

--- t2jscript.js
+++ t2jscript.js
@@ -1,10 +1,46 @@
+function Tab(title, url, favIconUrl){
+  this.title = title;
+  this.url = url;
+  this. favIconUrl = favIconUrl;
+  //this.hashtags = hashtags;
+}
+/*
+Tab.prototype.getHashTags = function () {
+  return this.hashtags;
+}
+*/
+function Window(id){
+  this.id = id;
+  this.tabs = new Array();
+}
+
+Window.prototype.pushtab = function (tab) {
+  var newtab = new Tab(tab.title, tab.url, tab.favIconUrl);
+  this.tabs.push(newtab);
+}
+
+Window.prototype.update(value){
+  var id = parseInt(value);
+  if (str.indexOf("win") >= 0){
+    
+  }
+}
+var sessionWin = new Array();
 
 /*
  * returns all open windows to callback function
 */
 function getAllWindows(callback){
           chrome.windows.getAll( {populate : true}, function (windows){
-            callback(windows);
+            for (var i = 0; i < windows.length; i++) {
+              sessionWin.push(new Window(windows[i].id) );
+              var tabs = windows[i].tabs;
+              for (var j = 0; j < tabs.length; j++) {
+                sessionWin[i].pushtab(tabs[j]);
+              };
+            };
+            //callback(windows);
+            callback(sessionWin);
           });
 }
 /*
@@ -19,19 +55,21 @@ function writeTabs (windows) {
   var d1=document.getElementById('t2');
   d1.innerHTML = '<div></div>'; // Empty div for start
   var winid = 0, tabid = 0;
-
+  var label = '<label style="margin: 1em 0 0 0;"><input type="checkbox" class="urlList" value=';
+  var winbutton =  '<button style="background:none;border:none;" id="winbuttonno"';
   for (var j= 0; j< windows.length; j++){
-    var label = '<label style="margin: 1em 0 0 0;"><input type="checkbox" class="urlList" value=';
     winid = "win" + j;
-    d1.insertAdjacentHTML('beforeend', '<div id='+winid+'>'+label+winid+'> Windows '+j+'</label></div>');    
-
+    d1.insertAdjacentHTML('beforeend','<div id='+winid+'>'+label+winid+'>'+winbutton+j+'> Windows '+j+'</button></label><br></div>');    
+    var winnode = document.getElementById(winid);
+    //winnode.onclick = function(){  alert("open window "+ j) };
     var tabs = windows[j].tabs;
-    tabid = winid;
     for(var i=0;i<tabs.length;i++) {
-      tabid += i;
+      tabid = winid+"tab"+i;
+      var favIcon = '<img height="16" width="16" hspace="8" src='+tabs[i].favIconUrl+'>  ';
       var atag = '<a href='+tabs[i].url+'>'+tabs[i].title+'</a>';
-      d1.insertAdjacentHTML('beforeend','<span id='+tabid+'>'+label+tabid+'>'+atag+'</label><br></span>');
+      winnode.insertAdjacentHTML('beforeend','<span id='+tabid+'>'+label+tabid+'>'+favIcon+atag+'</label><br></span>');
      }
+     //d1.insertAdjacentHTML('beforeend', '</div>');  //end jth windows div 
       updateTabsNumber(tabs.length);
     }
 }
@@ -91,6 +129,7 @@ function removeTabs(){
    var value = checkedhobbies[i].value;
    var selected = document.getElementById(value);
    selected.parentNode.removeChild(selected);
+   sessionWin.update(value);
   }
   updateTabsNumber(-1*checkedhobbies.length);
 }
@@ -110,10 +149,10 @@ function updateTabsNumber(length){
 /*
  * open the windows in new window
  */
-function openWindow(currentwindows){
+function openWindow(windows){
 
-  for(i=0;i<currentWindows.length;i++){
-   var tabs = currentWindows[i].tabs;
+  for(i=0;i <= windows.length;i++){
+   var tabs = windows[i].tabs;
    var arr = new Array();
    var len = 0;
    for(j=0; j<tabs.length;j++){
